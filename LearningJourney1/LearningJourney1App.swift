d40b7aeb2de8cb4ee099c4f991100b4ed059c54f
Aljowhara18 <Aljowhara18@gmail.com>
2025-10-23 00:42:05 +0300

    Initial Commit

---
+++ LearningJourney1/LearningJourney1App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourney1App.swift
+//  LearningJourney1
+//
+//  Created by Jojo on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourney1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningJourney1Tests/LearningJourney1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourney1Tests.swift
+//  LearningJourney1Tests
+//
+//  Created by Jojo on 23/10/2025.
+//
+
+import Testing
+@testable import LearningJourney1
+
+struct LearningJourney1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
